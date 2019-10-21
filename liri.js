ff2633f348c3c43890cbb9da4351331566c8d95c
Ana Cecilia Davis <anaceci01@Anas-iMac.local>
2019-10-21 00:25:25 -0700

    variable and packages

---
+++ liri.js
@@ -0,0 +1,18 @@
+// DOTENV 
+require('dotenv').config();
+
+//VARIABLES 
+let axios = require('axios');
+let Spotify = require ('node-spotify-api');
+let fs = require ('fs');
+let spotifyKeys = require('./keys.js');
+let spotify = new Spotify (spotifyKeys.spotify);
+let moment = require ('moment');
+
+
+//Function will take a song title and pull up 5 tracks from Spotify with that title, showing the artist, song name, Spotify preview lin, and the album.
+function spotifySong(songName) {
+    spotify.search({type: "track", query: songName, limit})
+}
+
+

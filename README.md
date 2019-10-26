63975ea6b890dfdcd6c93211dcec31a31ad953d0
Ana Cecilia Davis <anaceci01@Anas-iMac.local>
2019-10-25 23:21:26 -0700

    spotify fucntion

--- README.md
+++ README.md
@@ -6,3 +6,19 @@ Include screenshots, gifs or videos of the app functioning
 Contain a link to a deployed version of the app
 Clearly list the technologies used in the app
 State your role in the app development
+
+
+
+
+# Liri-Node-App
+
+Liri is a one-stop command line node app that takes in paramenters for songs, bands, and movies and returns data
+
+This app makes calls to the Spotify API, BandsInTown API,  and OMDB API search results
+
+When running this app in your command line, you may use the following to find your desired data:
+'spotify-this-song', 'concert-this', 'movie-this', and 'do-what-it-says'.
+
+
+***NOTE: If you would like to clone this repo, you will have to include your own.env file with your own unique Spotify ID and Secret. The format of your .env shoul look like below. Make sure to use your unique Spotify ID and Spotify Secret directly following the "-" below with no extra spaces.
+
--- liri.js
+++ liri.js
@@ -1,19 +1,58 @@
-// DOTENV 
+
+// Depenencies - DOTENV 
 require('dotenv').config();
 
-//VARIABLES 
-var keys = require("./keys");
+//Add the code required to import the `keys.js` file and store it in a variable.var keys = require("./keys");
+var keys = require("./keys.js");
 let axios = require('axios');
 let Spotify = require ('node-spotify-api');
 let fs = require ('fs');
-let spotifyKeys = require('./keys.js');
-let spotify = new Spotify (spotifyKeys.spotify);
 let moment = require ('moment');
+let spotifyKeys = require('./keys.js');
+let spotify = new Spotify (keys.spotify);
 
 
-//Function will take a song title and pull up 5 tracks from Spotify with that title, showing the artist, song name, Spotify preview lin, and the album.
-function spotifySong(songName) {
-    spotify.search({type: "track", query: songName, limit})
+var appCommand = process.argv [2];
+var userSearch = process.argv.slice(3).join(" ");
+//console.log("userSearch: " + appCommand ");
+
+
+// Make it so liri.js can take in one of the following commands:
+function liriStart(appCommand, userSearch) {
+    switch (appCommand) {
+        //spotify-this-song
+        case "spotify-this-song":
+            getSpotify(userSearch);
+            break;
+            //concert-this
+        case "concert-this":
+            getBandsInTown(userSearch);
+            break;
+            //movie-this
+        case "movie-this":
+            getOMBD(userSearch);
+            break;
+            do-what-it-says
+        case "do-what-it-says":
+            getRandom();
+            break;
+        default:
+            console.log("To continue, type one of the following commands: 'concert-this, 'spotify-this-song', 'movie-this', 'do-what-it-says' ");
+    }
+};
+
+// ------------------- Function to search Spotify API -------------------
+function getSpotify(songName) {
+    
+    if (songName === undefined){
+        songName = ""
+    }
+
+    spotify.search(
+        {
+            type: "track", 
+            query: songName, limit
+        })
 }
 
-
+`node liri.js concert-this <artist/band name here>`

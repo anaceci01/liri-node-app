3f6a07b9ce8692b4649a45a8294ca01336817809
Ana Cecilia Davis <anaceci01@Anas-iMac.local>
2019-10-28 22:26:11 -0700

    get bands in town function

--- liri.js
+++ liri.js
@@ -5,14 +5,13 @@ require('dotenv').config();
 //Add the code required to import the `keys.js` file and store it in a variable.var keys = require("./keys");
 var keys = require("./keys.js");
 let axios = require('axios');
-let Spotify = require ('node-spotify-api');
-let fs = require ('fs');
-let moment = require ('moment');
+let Spotify = require('node-spotify-api');
+let fs = require('fs');
+let moment = require('moment');
 let spotifyKeys = require('./keys.js');
-let spotify = new Spotify (keys.spotify);
 
 
-var appCommand = process.argv [2];
+var appCommand = process.argv[2];
 var userSearch = process.argv.slice(3).join(" ");
 //console.log("userSearch: " + appCommand ");
 
@@ -22,17 +21,16 @@ function liriStart(appCommand, userSearch) {
     switch (appCommand) {
         //spotify-this-song
         case "spotify-this-song":
-            getSpotify(userSearch);
+            spotifySong(userSearch);
             break;
-            //concert-this
+        //concert-this
         case "concert-this":
             getBandsInTown(userSearch);
             break;
-            //movie-this
+        //movie-this
         case "movie-this":
             getOMBD(userSearch);
             break;
-            do-what-it-says
         case "do-what-it-says":
             getRandom();
             break;
@@ -42,17 +40,46 @@ function liriStart(appCommand, userSearch) {
 };
 
 // ------------------- Function to search Spotify API -------------------
-function getSpotify(songName) {
-    
-    if (songName === undefined){
-        songName = ""
-    }
+function spotifySong(songName) {
+    let spotify = new Spotify(keys.spotify);
+    //console.log("Spotify key: " + spotify);
+
+    if (!songName) {
+        songName = "The Sign";
+    };
 
     spotify.search(
         {
-            type: "track", 
-            query: songName, limit
+            type: "track",
+            query: songName
+        },
+        function (err, data) {
+            if (err) {
+                return console.log('An error occurred: ' + err);
+            }
+
+            var songs = data.tracks.items
+            for (var i = 0; i < songs.length; i++) {
+                var spotifyResults =
+                "-----------------------------------------------------------------" +
+                    "Artist Name: " + data.tracks.items[i].album.artists[0].name +
+                    "Song Name: " + data.tracks.items[i].name +
+                    "Song Preview Link: " + data.tracks.items[i].href +
+                    "Album: " + data.tracks.items[i].album.name +
+                    console.log(spotifyResults);
+            }
         })
+
+function getBandsInTown(artist) {
+    var artist = userSearch;
+    var banQueryURL = "https://bandsintwon.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+    axios
+    .get(bandQueryURL)
+    .then(function(response){
+        console.log ("---------------------");
+        console.log("Name of venue: " + response.data)
+        var jsonData = 
+    })
 }
 
-`node liri.js concert-this <artist/band name here>`
